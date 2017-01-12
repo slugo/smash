9fc70aabe46bfb4141124dd74f92402f108f93bb
Sebastian Lugo <sebalugo@gmail.com>
2017-01-11 21:29:50 -0400

    Adding separate file for tournaments

---
+++ db/tournamentList.js
@@ -0,0 +1,21 @@
+const smashggTournaments = [
+    'fighting-fest-2016',
+    'the-big-house-6',
+    'mega-smash-mondays-80',
+    'mega-smash-mondays-79'
+]
+const challongeTournaments = [
+    'doassingles',
+    'Orangemaidcafe',
+    'fallensingles',
+    'mightysmash',
+    'spookyvillesingles',
+    'Androidsaga',
+    'hiroesingles',
+    'endofseasons',
+]
+
+module.exports = {
+    smashggTournaments,
+    challongeTournaments
+};
