827fbe8f0135cde5ea21970cc73bc9a07ff47056
Sebastian Lugo Nunez <sebalugo@gmail.com>
2017-01-12 14:30:27 -0400

    Fixing tournament list

--- db/tournamentList.js
+++ db/tournamentList.js
@@ -1,8 +1,6 @@
 const smashggTournaments = [
     'fighting-fest-2016',
-    'the-big-house-6',
-    'mega-smash-mondays-80',
-    'mega-smash-mondays-79'
+    'smash-gaiden-1'
 ]
 const challongeTournaments = [
     'doassingles',
