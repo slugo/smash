a8db00e9823b59e57816de939aaed99ddd3f641a
Sebastian Lugo Nunez <sebalugo@gmail.com>
2017-01-13 12:11:25 -0400

    Fixing model data type

--- db/models/tournament.js
+++ db/models/tournament.js
@@ -4,7 +4,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const tournamentSchema = mongoose.Schema({
 	name: String,
     date: Date,
-    particinpants: Integer,
+    participants: Number,
     placings: [ObjectId]
 });
 
