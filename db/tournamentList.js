a4387abab952a474d601600a9b13312a0558191d
Sebastian Lugo <sebalugo@gmail.com>
2017-01-15 17:14:50 -0400

    Updating tournament list

--- db/tournamentList.js
+++ db/tournamentList.js
@@ -1,6 +1,7 @@
 const smashggTournaments = [
     'fighting-fest-2016',
-    'smash-gaiden-1'
+    'smash-gaiden-1',
+    'smash-gaiden-the-cell-games',
 ]
 const challongeTournaments = [
     'doassingles',
